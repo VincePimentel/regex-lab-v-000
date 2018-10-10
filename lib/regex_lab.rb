f173410b0f94c2298219dd880cd7888c96c66531
Vince Andrew Pimentel <contact@vincep.me>
2018-10-10 19:30:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,9 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\b[aeiou]/)
-    return true
-  elsif word.scan(/[^aeiou]/)
-    return false
-  end
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.grep(/^uning$/)
+
 end
 
 def words_five_letters_long(text)
