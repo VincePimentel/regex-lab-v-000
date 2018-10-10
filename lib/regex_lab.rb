edb0ed2b845ef49fbc7c39d8c6d57127535e741e
Vince Andrew Pimentel <contact@vincep.me>
2018-10-10 19:47:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\d{10}/)
+  if phone.scan(/[\(| ]?\d{3}[\)| ]?\d{3}[-| ]?\d{4}\b/)
     return true
   else
     return false
